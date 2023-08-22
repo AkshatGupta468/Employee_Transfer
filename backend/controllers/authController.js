7e55482c48e5f0e5b0b4e631e01de2534f1e7f7e
AkshatGupta468 <gakshat468@gmail.com>
2023-08-22 19:39:11 +0530

    Merge

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -17,7 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-  });
+  })
 
   const token = signToken(newUser._id);
   res.status(201).json({
@@ -30,16 +30,16 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   //checking if email and password exist
   if (!email || !password) {
-    return next(new AppError("Please provide email and password", 400));
+    return next(new AppError("Please provide email and password", 400))
   }
 
   //checking if user exists and password is correct
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select("+password")
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect emial or password", 401));
+    return next(new AppError("Incorrect emial or password", 401))
   }
   //If everythimg is OK, send the token to client
   const token = signToken(user._id);
@@ -50,30 +50,30 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
-  let token;
+  let token
   // Getting token from client and checking if it exists
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(" ")[1]
   }
   if (!token) {
     return next(
       new AppError("You are not loggen in. Please log in to get access", 401)
-    );
+    )
   }
 
   //verification of the token
   //promisifying the verify function(using util library)
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
 
   //checking if the user still exists(token exists but user is deleted)s
-  const freshUser = await User.findById(decoded.id);
+  const freshUser = await User.findById(decoded.id)
   if (!freshUser) {
     return next(
       new AppError("The user belonging to this token no longer exists", 401)
-    );
+    )
   }
 
   //checking if the user has changed the password after token was created
@@ -83,7 +83,7 @@ exports.protect = catchAsync(async (req, res, next) => {
         "User recently changed password, please login getAllJSDocTagsOfKind",
         401
       )
-    );
+    )
   }
   //if everything's fine, granting access to the protected route
   req.user = freshUser;
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,35 @@
 // Create a basic express server
 // =============================================================
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const dotenv = require("dotenv")
+dotenv.config({ path: "./.env" })
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
-);
+)
 mongoose
   .connect(DB)
   .then(() => {
-    console.log("Connection to Database is successful");
+    console.log("Connection to Database is successful")
   })
   .catch((err) => {
-    console.log("Unable to Connect to Database", err);
-  });
+    console.log("Unable to Connect to Database", err)
+  })
 
-const app = require("./app");
+const app = require("./app")
 
-const portNo = process.env.PORT || 3000;
+const portNo = process.env.PORT || 3000
 
 const server = app.listen(portNo, () => {
-  console.log(`App running on port ${portNo}...`);
-});
+  console.log(`App running on port ${portNo}...`)
+})
 process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err);
-  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...")
+  console.log(err)
+  console.log(err.name, err.message)
   server.close(() => {
-    process.exit(1);
-  });
-});
+    process.exit(1)
+  })
+})

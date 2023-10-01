da6d6e6227c87a71d3b1ab063ee4318d9899b1e7
AkshatGupta468 <gakshat468@gmail.com>
2023-10-02 01:44:08 +0530

    changed routing to return user even if not complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,35 +1,38 @@
-const express = require("express");
-const multer = require("multer");
-const userController = require("./../controllers/userController");
-const authController = require("./../controllers/authController");
-const chatController = require("./../controllers/chatController");
+const express = require("express")
+const multer = require("multer")
+const userController = require("./../controllers/userController")
+const authController = require("./../controllers/authController")
+const chatController = require("./../controllers/chatController")
 
-const userRouter = express.Router();
-userRouter.post("/signup", authController.signup);
-userRouter.post("/login", authController.login);
-userRouter.post("/forgotPassword", authController.forgotPassword);
-userRouter.patch("/resetPassword/:token", authController.resetPassword);
-userRouter.use(authController.protect);
-userRouter.route('/profile').post(userController.postImmediateProfile);
-userRouter.use(userController.userInformationValidator);
+const userRouter = express.Router()
+userRouter.post("/signup", authController.signup)
+userRouter.post("/login", authController.login)
+userRouter.post("/forgotPassword", authController.forgotPassword)
+userRouter.patch("/resetPassword/:token", authController.resetPassword)
 
+userRouter.use(authController.protect)
 // Protected Routes
-
-userRouter.patch("/updatePassword", authController.updatePassword);
+userRouter
+  .route("/allUsers")
+  .get(authController.restrictTo("admin"), userController.getAllUsers)
 
 userRouter
   .route("/profile")
   .get(userController.getDetails)
-  .patch(userController.uploadUserPhoto, userController.updateMe);
- 
+  .patch(userController.uploadUserPhoto, userController.updateMe)
+
+userRouter.patch("/updatePassword", authController.updatePassword)
+
+userRouter.use(userController.userInformationValidator)
+
 userRouter
   .route("/employees")
-  .get(userController.userInformationValidator,authController.protect, userController.getEmployees);
+  .get(
+    userController.userInformationValidator,
+    authController.protect,
+    userController.getEmployees
+  )
 
-userRouter
-  .route("/allUsers")
-  .get(authController.restrictTo("admin"), userController.getAllUsers);
-
-userRouter.route("/chat").get(chatController.getChat);
-userRouter.route("/message").post(chatController.newMessage);
-module.exports = userRouter;
+userRouter.route("/chat").get(chatController.getChat)
+userRouter.route("/message").post(chatController.newMessage)
+module.exports = userRouter
